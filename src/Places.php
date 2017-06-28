d325b0954d9128457132f05786dde5b3badd912f
Calla Rudolph and Max Scher <calla@>
2017-06-28 08:27:32 -0700

    define class with getters and setters

---
+++ src/Places.php
@@ -0,0 +1,58 @@
+<?php
+    class Place {
+        private $city_name;
+        private $duration;
+        private $best_memory;
+        private $best_meal;
+        private $favorite_abductee;
+
+        function __construct($city_name, $duration, $best_memory, $best_meal, $favorite_abductee) {
+
+            $this->city_name = $city_name;
+            $this->duration = $duration;
+            $this->best_memory = $best_memory;
+            $this->best_meal = $best_meal;
+            $this->favorite_abductee = $favorite_abductee;
+        }
+
+        function setCity() {
+            $this->city_name = $city_name;
+        }
+
+        function getCity() {
+            return $this->city_name;
+        }
+
+        function setDuration() {
+            $this->duration = $duration;
+        }
+
+        function getDuration() {
+            return $this->duration;
+        }
+
+        function setMemory() {
+            $this->best_memory = $best_memory;
+        }
+
+        function getMemory() {
+            return $this->best_memory;
+        }
+
+        function setMeal() {
+            $this->best_meal = $best_meal;
+        }
+
+        function getMeal() {
+            return $this->best_meal;
+        }
+
+        function setAbductee() {
+            $this->favorite_abductee = $favorite_abductee;
+        }
+
+        function getAbductee() {
+            return $this->favorite_abductee;
+        }
+    }
+?>
